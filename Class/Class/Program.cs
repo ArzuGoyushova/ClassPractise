559a3d91ffd94b2338dc03168db463a9d4d78c98
ArzuGoyushova <arzusg@code.edu.az>
2022-11-13 19:14:43 +0400

    feat: Class Practise

---
+++ Class/Class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Request req1 = new Request();
+            req1.Name = "Arzu";
+            req1.Surname = "Goyushova";
+            req1.PhoneNumber = "0708963496";
+            req1.Category = "Full-Stack Programming";
+            req1.Notes = "No comment";
+
+            
+            Console.WriteLine(req1.Credentials());
+            Console.WriteLine(req1.PhNumber());
+            Console.WriteLine(req1.Cat());
+            Console.WriteLine(req1.Note());
+
+        }
+
+
+
+    }
+}
---
+++ Class/Class/Request.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    internal class Request
+    {
+        public string Name;
+        public string Surname;
+        public string PhoneNumber;
+        public string Category;
+        public string Notes;
+
+        public Request()
+        {
+            Console.WriteLine("working");
+        }
+        public Request(string name, string surname, string phone, string category, string notes)
+        {
+            Name = name;
+            Surname = surname;
+            PhoneNumber = phone;
+            Category = category;
+            Notes = notes;
+        }
+       
+
+        public string Credentials()
+        {
+            return $"Ad: {Name} , soyad: {Surname}";
+        }
+
+        public string PhNumber()
+        {
+            return $"Nomre {PhoneNumber}";
+        }
+
+        public string Cat()
+        {
+            return $"Category: {Category}";
+        }
+
+        public string Note()
+        {
+            return $"Notes: {Notes}";
+        }
+
+
+
+    }
+}
