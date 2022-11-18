f798b98f2c6ee16cf82980f4c39b015d4965a2bd
Arzu <115929474+ArzuGoyushova@users.noreply.github.com>
2022-11-18 21:55:59 +0400

    feat: Lab practice is added.

---
+++ ClassLab/ClassLab/ArrayClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ClassLab
+{
+
+    internal class ArrayClass
+    {
+        //public int[] Numbers;
+        //public void ArrMethod ()
+        //{
+        //    foreach (int item in Numbers)
+        //    {
+        //        Console.WriteLine($"{item}"); 
+        //    }
+            
+        //}
+        
+        //public void Dioganal (int[,] array)
+        //{
+        //    int sum1 = 0;
+        //    int sum2 = 0;
+        //    int diff = 0;
+        //    int [,] array2=new int[3,3];
+        //    for (int i=0; i < array.GetLength(0); i++)
+        //    {
+        //        sum1 = sum1 + array[i, i];
+        //    }
+            
+        //    int j=array.GetLength(0)-1;
+        //    for (int i=0; i < array.GetLength(0); i++)
+        //    {               
+        //        sum2=sum2+array[i,j];
+        //        j--;
+        //    }
+        //    diff = sum1 - sum2;
+        //    Console.WriteLine(sum1);
+        //    Console.WriteLine(sum2); 
+        //    Console.WriteLine(diff);
+        //}
+
+        public string Substring (string word)
+        {
+            string word2 = word.Substring();
+            return word2;
+        }
+    }
+}
---
+++ ClassLab/ClassLab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayClass arrObj = new ArrayClass();
+            //arrObj.Numbers =new int [] {1,2,3,4};
+            //arrObj.ArrMethod();           
+
+            //ArrayClass arrObj = new ArrayClass();
+            //int [,] nums = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 15 } };
+            //arrObj.Dioganal(nums);
+
+            ArrayClass subs = new ArrayClass();
+            Console.WriteLine(subs.Substring(" P515 "));
+        }
+    }
+}
