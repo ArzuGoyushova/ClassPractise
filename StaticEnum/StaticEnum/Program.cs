ca83018abfdfed33c03c1878c01e06c47e8565d8
Arzu <115929474+ArzuGoyushova@users.noreply.github.com>
2022-11-26 18:08:10 +0400

    feat: Static task is added.

---
+++ StaticEnum/StaticEnum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace StaticEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 4;
+            //Console.WriteLine(num.Factorial());
+
+            //string sentence = "Cause I I I know what you like boy.";
+            //Console.WriteLine(sentence.Count("I"));
+
+            Console.WriteLine("Add a sentence: ");
+            string sentence=Console.ReadLine();
+            Console.Write("The word you search: ");
+            string search = Console.ReadLine();
+            Console.WriteLine("Count = "+sentence.Count(search));
+        }
+    }
+}
---
+++ StaticEnum/StaticEnum/Static.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StaticEnum
+{
+    static class Static
+    {
+        public static int Factorial(this int num)
+        {
+            int fact = 1;
+            for (int i = num; i > 1; i--)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+
+        //public static int Count(this string sentence, string search)
+        //{
+        //    int count = 0;
+        //    string[] words = sentence.Split(' ');
+        //    foreach(string item in words)
+        //    {
+        //        if (item == search)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+        public static int Count(this string sentence, string search)
+        {
+           return Regex.Matches(sentence, search).Count;
+        }
+    }
+}
