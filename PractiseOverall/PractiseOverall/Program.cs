7e8adde0687dd893499466122989c5dd915c0794
ArzuGoyushova <115929474+ArzuGoyushova@users.noreply.github.com>
2022-11-30 20:04:57 +0400

    refactor: updated.

--- PractiseOverall/PractiseOverall/Program.cs
+++ PractiseOverall/PractiseOverall/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using ClassLibrary1;
 
 namespace PractiseOverall
@@ -9,19 +10,44 @@ namespace PractiseOverall
     {
         static void Main(string[] args)
         {
-            string name="Arzu Goyushova";
-            Console.WriteLine(Reversed(name));
-           
+            //string name="Arzu Goyushova";
+            //Console.WriteLine(Reversed(name));
 
+            //Calc(4,2);
+            Console.WriteLine(RegexCheck("her ne45yse 123"));
         }
-        static string Reversed(string name)
+
+        #region ReversedString
+        //static string Reversed(string name)
+        //{
+        //  StringBuilder sb = new StringBuilder();
+        //    for (int i = name.Length - 1; i >= 0; i--)
+        //    {
+        //        sb.Append(name[i]);
+        //    }
+        //    return sb.ToString();
+        //}
+        #endregion
+
+        #region Math
+        //static void Calc (int x, int y)
+        //{
+        //    int min=Math.Min(x,y);
+        //    int max=Math.Max(x,y);
+        //    int abs=Math.Abs(x);
+        //    double pow = Math.Pow(x,y);
+        //    double sqrt = Math.Sqrt(x);
+        //    Console.WriteLine(pow + sqrt);
+        //}
+        #endregion
+
+        #region Regex
+        static int RegexCheck(string sentence)
         {
-          StringBuilder sb = new StringBuilder();
-            for (int i = name.Length - 1; i >= 0; i--)
-            {
-                sb.Append(name[i]);
-            }
-            return sb.ToString();
+            return Regex.Matches(sentence, @"\d+").Count;
+            //return Regex.IsMatch(sentence, @"\d");
         }
+
+        #endregion
     }
 }
