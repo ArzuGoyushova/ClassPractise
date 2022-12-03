6f604a346e8a8b1a40a7a5a2582a8e446fec7e67
ArzuGoyushova <115929474+ArzuGoyushova@users.noreply.github.com>
2022-12-03 18:35:49 +0400

    feat: 12.03 Practicew

---
+++ Practise/Service/Helpers/AccountConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Helpers
+{
+    public class AccountConstants
+    {
+        public const string SuperAdminValid = "SuperAdmin girishi ugurludur.";
+        public const string SuperAdminInvalid = "Girish ugursuzdur";
+        public const string PasswordOrEmailInvalid = "Email ve ya password sehvdir";
+    }
+}
---
+++ Practise/Service/Services/Account.cs
@@ -0,0 +1,36 @@
+﻿using Service.Enum;
+using Service.Helpers;
+using Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public class Account : IAccount
+    {
+        public string Login(string email, string password, int role)
+        {
+            if (email=="test@code.edu.az" && password == "test12345")
+            {
+                if (CheckRoles(role))
+                {
+                    return AccountConstants.SuperAdminValid;
+                }
+                else
+                {
+                    return AccountConstants.SuperAdminInvalid;
+                }
+            }
+            else
+            {
+                return AccountConstants.PasswordOrEmailInvalid;
+            }
+        }
+
+        public bool CheckRoles(int role)
+        {
+            return role == (int)Roles.SuperAdmin;
+        }
+    }
+}
