2291ea01bd0fa8fd52bbe86fce982b97f3b01344
ArzuGoyushova <115929474+ArzuGoyushova@users.noreply.github.com>
2022-12-04 19:39:14 +0400

    feat: 04.12 Task

---
+++ PersonArr/PersonArr/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonArr
+{
+    public class Person : IComparable
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public DateTime Birthday { get; set; }
+        public Person(string name, string surname, int age, int salary)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Salary = salary;
+        }
+        public static bool operator <(Person p1, Person p2)
+        {
+            return p1.Age < p2.Age;
+        }
+        public static bool operator >(Person p1, Person p2)
+        {
+            return p1.Age > p2.Age;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return this.Age.CompareTo(((Person)obj).Age);
+            
+        }
+
+      
+    }
+}
---
+++ PersonArr/PersonArr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace PersonArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Arzu", "Goyushova", 26, 10);
+            Person person2 = new Person("Taehyung", "Kim", 30, 1000000);
+            Person person3 = new Person("Narmin", "Ismayilova", 19, 1000000000);
+            Person[] arr = { person1, person2, person3 };
+
+
+            Array.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
